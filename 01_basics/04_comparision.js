a66ede64ea2dd9a1b66aa77d5b1ee1dab3bcbde5
Amogh Pal <81255914+amogh9999@users.noreply.github.com>
2026-04-20 15:08:44 +0000

    compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//console.log("2" > 1); // js automatically convert the string into number
+//console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // true why beacuse js convert the null into 0
+console.log(null == 0);
+
+console.log(undefined == 0);
+
+// ===
+console.log("2" == 2); // true
+console.log("2" === 2); // false
