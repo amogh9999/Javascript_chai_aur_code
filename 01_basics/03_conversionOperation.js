0e7478347f72592766f81b365bd74685f203f3d5
Amogh Pal <81255914+amogh9999@users.noreply.github.com>
2026-04-20 13:38:08 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33ac"
+
+console.log(typeof score); // number 
+console.log(typeof(score)); // number
+ 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // NaN not a number
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false 
+// "hitesh" => ture
+
+let someNumber = 33
+console.log(someNumber);
+console.log(typeof someNumber);
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
